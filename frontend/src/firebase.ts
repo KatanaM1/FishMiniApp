8cf3041dcbbbdae3d359122a4ab53cf7e5019fbc
Mikhail Polyakov <avtomik-s@yandex.ru>
2026-08-13 02:48:06 +0300

    Fix apiKey in firebase config

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -7,13 +7,14 @@ import type { Firestore } from "firebase/firestore";
 import type { Auth } from "firebase/auth";
 
 // Твой конфиг
-const firebaseConfig = {
-  apiKey: "....",
-  authDomain: "....",
-  projectId: "....",
-  storageBucket: "....",
-  messagingSenderId: "....",
-  appId: "...."
+const firebaseConfig = { 
+  apiKey : "AIzaSyASJb-bOkj1V0Zoxlu8STmj3e2nndTMmrM" , 
+  authDomain : "storefishminiapp.firebaseapp.com" , 
+  projectId : "storefishminiapp" , 
+  storageBucket : "storefishminiapp.firebasestorage.app" , 
+  messagingSenderId : "248863675377" , 
+  appId : "1:248863675377:web:2ff562e57e9372f0a46a50" , 
+  measurementId : "G-QLLWNLTMBH" 
 };
 
 // Инициализация
